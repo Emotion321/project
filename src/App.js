0648f9f7782cb245c1c6d0d8fb00fbedec605660
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-07-28 18:10:12 +0500

    counting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const increase = () => {
+        setCount(count + 1);
+    };
+    const decrease = () => {
+        setCount(count - 1);
+    };
+    const zero = () => {
+        setCount(0);
+    };
   return (
-    <div className="App"> Hello World! </div>
+    <div className="App">
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+        <button onClick={zero}>Set to Zero</button>
+
+        {count}
+    </div>
   );
 }
 
