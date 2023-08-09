2a94dc392dc70de1a1b1954b0400b96b664841e9
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-09 19:12:11 +0500

    finally figured out how to use DOM

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Home } from "./pages/Home";
 import React from "react";
 
 function App() {
-  const [generatedExcuse, setGeneratedExcuse] = useState("");
-
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
-      (res) => {
-        setGeneratedExcuse(res.data[0].excuse);
-      }
-    );
-  };
-
   return (
     <div className="App">
-      <h1> Generate an excuse </h1>
-      <button onClick={() => fetchExcuse("office")}> Office </button>
-      <button onClick={() => fetchExcuse("funny")}> Funny </button>
-      <button onClick={() => fetchExcuse("unbelievable")}>
-        {" "}
-        Unbelievable{" "}
-      </button>
-
-      <h2>{generatedExcuse}</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
