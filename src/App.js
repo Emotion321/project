78f3bf75783525cc922d299bce81911d979e2054
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-11 12:20:09 +0500

    More DOM stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
-import { Home } from "./pages/Home"; 
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
 import React from "react";
 
 function App() {
@@ -10,6 +12,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </div>
