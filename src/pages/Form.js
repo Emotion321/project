994fdae50144fd44ac34dddc2879c5815af6b097
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-17 13:41:58 +0500

    form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -4,10 +4,15 @@ export const Form = () => {
     return (
         <form>
             <input type="text" placeholder="Full Name" />
+            <br />
             <input type="text" placeholder="Email" />
+            <br />
             <input type="text" placeholder="Age" />
+            <br />
             <input type="password" placeholder="Password" />
+            <br />
             <input type="password" placeholder="Confirm Password" />
+            <br />
             <input type="submit" />
         </form>
     )
