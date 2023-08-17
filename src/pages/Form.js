b1462d981dde2158adb417f42b60d95629ec6fcc
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-17 13:45:48 +0500

    form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -2,8 +2,13 @@ import { useForm } from "react-hook-form";
 import React from "react";
 
 export const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = () => {
+    
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Full Name" />
       <br />
       <input type="text" placeholder="Email" />
