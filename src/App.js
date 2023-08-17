e763a39091533c7eb792ce58a9625e53aed3d23b
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-17 13:35:36 +0500

    form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import React, { createContext } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const AppContext = createContext();
+import React from "react";
 
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    }
-  }});
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>404</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
-  );
+  )
 }
 
 export default App;
