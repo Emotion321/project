f3661e0667bbda379089f173ec0d186571f5fb30
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-17 13:43:07 +0500

    form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,19 +1,20 @@
+import { useForm } from "react-hook-form";
 import React from "react";
 
 export const Form = () => {
-    return (
-        <form>
-            <input type="text" placeholder="Full Name" />
-            <br />
-            <input type="text" placeholder="Email" />
-            <br />
-            <input type="text" placeholder="Age" />
-            <br />
-            <input type="password" placeholder="Password" />
-            <br />
-            <input type="password" placeholder="Confirm Password" />
-            <br />
-            <input type="submit" />
-        </form>
-    )
-}
+  return (
+    <form>
+      <input type="text" placeholder="Full Name" />
+      <br />
+      <input type="text" placeholder="Email" />
+      <br />
+      <input type="text" placeholder="Age" />
+      <br />
+      <input type="password" placeholder="Password" />
+      <br />
+      <input type="password" placeholder="Confirm Password" />
+      <br />
+      <input type="submit" />
+    </form>
+  );
+};
