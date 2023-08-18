9808b6cb3c78fa34993cef5f3a9019e7f178f3f4
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-18 09:32:19 +0500

    some changes

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,23 +1,56 @@
-import { useForm } from "react-hook-form";
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup
+      .string()
+      .email()
+      .required(),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .min(18)
+      .required(),
+    password: yup
+      .string()
+      .min(8)
+      .max(20)
+      .required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null])
+      .required(),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const onSubmit = () => {
-    
-  }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Full Name" />
-      <br />
-      <input type="text" placeholder="Email" />
-      <br />
-      <input type="text" placeholder="Age" />
-      <br />
-      <input type="password" placeholder="Password" />
-      <br />
-      <input type="password" placeholder="Confirm Password" />
+      <input type="text" placeholder="Full Name" {...register("fullName")} />
+
+      <p>{errors.fullName}</p>
+      <input type="text" placeholder="Email" {...register("email")} />
+      <input type="number" placeholder="Age" {...register("age")} />
+      <input type="password" placeholder="Password" {...register("password")} />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        {...register("confirmPassword")}
+      />
       <br />
       <input type="submit" />
     </form>
