650892b9e604b11598bc8d6a10d3b7514da394b7
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-20 22:16:06 +0500

    done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className="App">
+      <button onClick={() => setIsVisible((prev) => !prev)}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h2> Hidden Text </h2>}
     </div>
   )
 }
