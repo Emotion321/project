bedcf5eb5331857754cd9ff9e2591ab155aea5ba
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-21 13:04:16 +0500

    toggle

--- src/useToggle.js
+++ src/useToggle.js
@@ -1,5 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 
-export const useToggle = () => {
-    const [isVisible, setIsVisible] = useState(false);
-}
+export const useToggle = (initialVal = false) => {
+  const [state, setState] = useState(initialVal);
+
+  const toggle = () => {
+    setState((prev) => !prev)
+  }
+
+  return [state, toggle]
+};
