c9fa1d620f020bc094f635cf285bc9bec9ed228a
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-08-21 13:07:20 +0500

    check

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  const [isVisibl, toggle] = useToggle()
+  const [isVisibl, toggle] = useToggle();
 
   return (
     <div className="App">
-      <button onClick={() => setIsVisible((prev) => !prev)}>
-        {isVisible ? "Hide" : "Show"}
-      </button>
+      <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
       {isVisible && <h2> Hidden Text </h2>}
     </div>
-  )
+  );
 }
 
 export default App;
