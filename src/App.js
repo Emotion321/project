2c3e16531d48067692ce04fa5a431e7b9d10d13a
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-09-09 13:21:43 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
 import React from "react";
-import { Cat } from "./Cat";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 function App() {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: true,
-      },
-    },
-  });
-
-  return (
-    <div className="App">
-      <QueryClientProvider client={client}>
-        <Cat />
-      </QueryClientProvider>
-    </div>
-  );
+  return;
 }
 
 export default App;
