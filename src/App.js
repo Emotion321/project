3e03b39678f7da0c153e907bd62213b4555ff4be
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-09-09 15:35:54 +0500

    counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+
 function App() {
-  return;
+  const [num, setNum] = useState(0);
+
+  const counterPlus = () => {
+    setNum(num + 1);
+  };
+
+  const counterMinus = () => {
+    setNum(num - 1);
+  };
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={counterPlus}>+</button>
+      <button onClick={counterMinus}>-</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/counterApp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const counterPlus = () => {
+    return num + i++
+}
+
+const counterMinus = () => {
+    return num + i--
+}
