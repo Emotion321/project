15536b134bca32082e7e4d02b886d0280c9f96c3
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-10-02 17:38:41 +0500

    forms with set up yup

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,12 +1,29 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const CreateForm = () => {
-  return (
-    <form>
-      <input placeholder="Title..." />
-      <input placeholder="Description..." />
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add a title"),
+    description: yup
+      .string()
+      .required("Your post must have a description of at least 5 characters long")
+      .min(5, "Description must be at least 5 characters long"),
+  });
 
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onCreatePost = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onCreatePost)}>
+      <input placeholder="Title..." {...register("title")} />
+      <textarea placeholder="Description..." {...register("description")} />
+      <input type="submit" />
     </form>
   );
 };
--- src/pages/create-post/create-post.tsx
+++ src/pages/create-post/create-post.tsx
@@ -3,9 +3,9 @@ import { CreateForm } from "./create-form";
 export const CreatePost = () => {
   return (
     <>
-      <form>
+      <>
         <CreateForm />
-      </form>
+      </>
     </>
   );
 };
