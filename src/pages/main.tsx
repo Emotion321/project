18f6977169dc797841ef3291c667a016df012933
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-10-27 17:00:56 +0500

    working posts

--- src/pages/main.tsx
+++
@@ -1,7 +0,0 @@
-import { getDocs, collection } from "firebase/firestore";
-import { database } from "../config/firebase";
-
-export const Main = () => {
-  const postsRef = collection(database, "posts");
-  return <div> Home Page </div>;
-};
