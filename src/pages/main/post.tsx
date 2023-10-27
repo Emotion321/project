6ab413a25ba8fd67d817dbddd37e2649b117b1ce
Laziz <104445440+Emotion321@users.noreply.github.com>
2023-10-27 18:05:55 +0500

    likes

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,4 +1,7 @@
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { Post as IPost } from "./main";
+import { auth, database } from "../../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface Props {
   post: IPost;
@@ -6,6 +9,20 @@ interface Props {
 
 export const Post = (props: Props) => {
   const { post } = props;
+  const [user] = useAuthState(auth);
+  
+
+  const likesRef = collection(database, "likes");
+
+  const likesDoc = query(likesRef, where("postId", "==", post.id) )
+
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc)
+  }
+
+  const addLike = async () => {
+    await addDoc(likesRef, { userId: user?.uid, postId: post.id });
+  };
   return (
     <div>
       <div className="title">
@@ -16,6 +33,8 @@ export const Post = (props: Props) => {
       </div>
       <div className="footer">
         <p>- {post.username}</p>
+        <button onClick={addLike}>&#128077;</button>
+        <p>Likes: {} </p>
       </div>
     </div>
   );
